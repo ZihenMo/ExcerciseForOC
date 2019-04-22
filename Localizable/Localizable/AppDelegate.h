9aa9f8c3d723f649b12235b3fbd1c4dce26ce588
muzimo <muzimo@gshopper.com>
2019-04-22 14:11:18 +0800

    Add the Localizable Project.

---
+++ Localizable/Localizable/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Localizable
+//
+//  Created by gshopper on 2019/4/22.
+//  Copyright © 2019 mozihen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Localizable/Localizable/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Localizable
+//
+//  Created by gshopper on 2019/4/22.
+//  Copyright © 2019 mozihen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
